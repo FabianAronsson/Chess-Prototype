aa7ebb80fa5ba9d8b520640641feaa98a8a71729
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2021-03-23 15:59:30 +0100

    Visual Representation of Chess
    Underlying logic should be next.

--- Chess/Chess/MainWindow.xaml.cs
+++ Chess/Chess/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,115 @@ namespace Chess
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<ImageBrush> WhiteImages = new List<ImageBrush>();
+        public List<ImageBrush> BlackImages = new List<ImageBrush>();
         public MainWindow()
         {
             InitializeComponent();
+            SetImages();
+            CreatePieces();
+        }
+
+
+        public void CreatePieces()
+        {
+            int indexForWhite = 0;
+            int indexForBlack = 0;
+            //Black Pieces
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == 8)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        Button piece = new Button
+                        {
+                            Background = BlackImages[8],
+                            BorderThickness = new Thickness(0)
+                        };
+                        Grid.SetRow(piece, 1);
+                        Grid.SetColumn(piece, j);
+                        Board.Children.Add(piece);
+                    }
+                }
+                else
+                {
+                    Button piece = new Button
+                    {
+                        Background = BlackImages[indexForBlack],
+                        BorderThickness = new Thickness(0)
+                    };
+                    Grid.SetRow(piece, 0);
+                    Grid.SetColumn(piece, i);
+                    Board.Children.Add(piece);
+                    indexForBlack++;
+                }
+            }
+
+            //White Pieces
+            for (int i = 47; i < 63; i++)
+            {
+                if (i == 47)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+
+                        Button piece = new Button
+                        {
+                            Background = WhiteImages[8],
+                            BorderThickness = new Thickness(0)
+                        };
+                        Grid.SetRow(piece, 6);
+                        Grid.SetColumn(piece, j);
+                        Board.Children.Add(piece);
+                    }
+                }
+                else
+                {
+                    Button piece = new Button
+                    {
+                        Background = WhiteImages[indexForWhite],
+                        BorderThickness = new Thickness(0)
+                    };
+                    Grid.SetRow(piece, 7);
+                    Grid.SetColumn(piece, indexForWhite);
+                    Board.Children.Add(piece);
+                    indexForWhite++;
+                }
+                if (indexForWhite == 8)
+                {
+                    break;
+                }
+            }
+
+
+
+        }
+
+        public void SetImages()
+        {
+            List<string> ImagePathsForWhite = new List<string>() { "../../../ChessImages/wRook.png", "../../../ChessImages/wKnight.png",
+                "../../../ChessImages/wBishop.png", "../../../ChessImages/wQueen.png", "../../../ChessImages/wKing.png", "../../../ChessImages/wBishop.png",
+                "../../../ChessImages/wKnight.png", "../../../ChessImages/wRook.png", "../../../ChessImages/wPawn.png",  };
+            List<string> ImagePathsForBlack = new List<string>() { "../../../ChessImages/Rook.png", "../../../ChessImages/Knight.png",
+                "../../../ChessImages/Bishop.png", "../../../ChessImages/Queen.png", "../../../ChessImages/King.png", "../../../ChessImages/Bishop.png",
+                "../../../ChessImages/Knight.png", "../../../ChessImages/Rook.png", "../../../ChessImages/Pawn.png",  };
+            for (int i = 0; i < 9; i++)
+            {
+                var brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri(ImagePathsForWhite[i], UriKind.Relative));
+                WhiteImages.Add(brush);
+
+
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                var brushForBlack = new ImageBrush();
+                brushForBlack.ImageSource = new BitmapImage(new Uri(ImagePathsForBlack[i], UriKind.Relative));
+                BlackImages.Add(brushForBlack);
+            }
         }
     }
 }
+
+
